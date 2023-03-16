cf9fc30cd69062b70da79782a775e470158f8f79
LordAkirast <darkwarish@gmail.com>
2023-03-16 19:01:01 -0300

    Tela de habitos praticamente concluída. Próximo passo é pegar imagem de perfil com usecontext

--- src/components/Habits.js
+++ src/components/Habits.js
@@ -10,12 +10,15 @@ export default function Habits() {
             <NavBar>trackit <img src="a" alt="a"></img></NavBar>
             <HomeDiv>
                 <HabitsDiv>
-                    <div>Meus Habitos +</div>
-                    <p>Você não tem nenhum habito cadastrado</p>
+                    <MyHabits>
+                        <div>Meus Habitos</div> <p>+</p>
+                    </MyHabits>
+                    <p>Você não tem nenhum hábito <br/> cadastrado ainda. Adicione um hábito <br/> para começar a trackear!</p>
+
                 </HabitsDiv>
 
             </HomeDiv>
-            <Footer>Habitos | Icone | Histórico</Footer>
+            <Footer>Habitos | <p>Icone</p> | Histórico</Footer>
         </>
 
     )
@@ -26,29 +29,84 @@ width: 375px;
 height: 75px;
 background-color: #126BA5;
 justify-content: space-around;
+align-items: center;
 display: flex;
 color: white;
 position: fixed;
 top: 0;
+font-family: 'Playball', cursive;
+font-size: 38px;
 
 `
 
 const HomeDiv = styled.div`
 width: 375px;
 height: 667px;
-background-color: aqua;
+background-color: #F2F2F2;
 display: flex;
 flex-direction: column;
 /* justify-content: center; */
 align-items: flex-start;
-
 `
 
 const HabitsDiv = styled.div`
-margin-top: 75px`
+margin-top: 75px;
+
+p {
+    margin-left: 15px;
+    color: #666666;
+}`
+
+const MyHabits = styled.div`
+display: flex;
+justify-content: space-around;
+align-items: baseline;
+font-family: 'Lexend Deca', sans-serif;
+width: 375px;
+color: #126BA5;
+font-size: 20px;
+
+
+p{
+    width: 40px;
+    height: 35px;
+    background-color: #52B6FF;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+    font-size: 26px;
+    display: flex;
+    margin-left: 15px;
+    border-radius: 5px;
+}
+`
+
 
 const Footer = styled.div`
 width: 375px;
 height: 75px;
+background-color: white;
+justify-content: space-around;
+align-items: center;
+display: flex;
+color: white;
+position: fixed;
+bottom: 0;
+font-family: 'Lexend Deca', sans-serif;
+font-size: 20px;
+color: #52B6FF;
+
+p {
+    background-color: red;
+    width: 91px;
+    height: 91px;
+    border-radius: 100px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 60px;
+}
 `
 
