880b48c02fdbc0abd1d7cf674e1aab5865efa190
LordAkirast <darkwarish@gmail.com>
2023-03-16 09:41:28 -0300

    Agora as cores das telas de cadastro e login estão completas. Próximo passo, implementar a lógica de cadastro.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import axios from "axios";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import styled from "styled-components";
 import Home from "./components/Home";
+import SignUp from "./components/SignUp";
 
 export default function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/cadastro" element={<SignUp />} />
             </Routes>
         </BrowserRouter>
     )
 }
+
+
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,36 +1,35 @@
 import axios from "axios";
 import { useState } from "react";
-import { BrowserRouter, routes, route, Form, } from "react-router-dom";
+import { BrowserRouter, routes, route, Form } from "react-router-dom";
 import styled from "styled-components";
 import Logo from './images/Logo.png';
-import { Link } from "react-router-dom";
 
-
-export default function Home() {
+export default function SignUp() {
     const [login, setlogin] = useState('')
     const [password, setpassword] = useState('')
-    return (
-        <HomeDiv>
-            <ImgLogo src={Logo} alt="Logo"></ImgLogo>
+    const [name, setname] = useState('')
+    const [photo, setphoto] = useState('')
+return (
+    <HomeDiv>
+        <ImgLogo src={Logo} alt="Logo"></ImgLogo>
 
-            <Forms onSubmit={Login}>
-                <input onChange={e => setlogin(e.target.value)} id='login' type='text' value={login} placeholder='Usuário' />
+
+        <Forms onSubmit={Signup}>
+                <input onChange={e => setlogin(e.target.value)} id='login' type='text' value={login} placeholder='Email' />
                 <input onChange={e => setpassword(e.target.value)} id='password' type='text' value={password} placeholder='Senha' />
-
+                <input onChange={e => setname(e.target.value)} id='name' type='text' value={name} placeholder='Nome' />
+                <input onChange={e => setphoto(e.target.value)} id='photo' type='text' value={photo} placeholder='Foto' />
                 <button id="button" type="submit">Entrar</button>
             </Forms>
-            <Link to='/cadastro'>
-                <a>Não tem uma conta? Cadastre-se</a>
-            </Link>
-        </HomeDiv>
-    )
 
-    function Login() {
-        alert('Entrou')
-    }
+    </HomeDiv>
+)
 
+function Signup() {
+    alert('Cadastrou')
 }
 
+}
 
 const HomeDiv = styled.div`
 width: 375px;
@@ -40,10 +39,6 @@ display: flex;
 flex-direction: column;
 justify-content: center;
 align-items: center;
-
- a{
-    color: #52B6FF
- }
 `
 
 const ImgLogo = styled.img`
